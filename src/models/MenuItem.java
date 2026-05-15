7cec18e4aad3a67542a24fd0c451d2eb5021d048
Moiz Ahmad <moizahmad924@gmail.com>
2026-05-15 19:56:37 +0500

    Created the MenuItem, OrderItem and Order models with some relevant enums

---
+++ src/models/MenuItem.java
@@ -0,0 +1,94 @@
+package models;
+import enums.Category;
+
+class MenuItem{
+    private String itemName;
+    private String menuItemID;
+    private double price;
+    private String description;
+    private Category category;
+    private String imageUrl;
+    private boolean isAvailable;
+    private int preparationTime;
+    private int servingSize;
+    public MenuItem(String itemName, String menuItemID, double price, String description, Category category, String imageUrl, int preparationTime, int servingSize) {
+        setItemName(itemName);
+        this.menuItemID = menuItemID;
+        setPrice(price);
+        setDescription(description);
+        setCategory(category);
+        setImageUrl(imageUrl);
+        setIsAvailable();
+        setPreparationTime(preparationTime);
+        setServingSize(servingSize);
+    }
+
+    
+    public String getItemName() {return itemName;}
+    public String getID() {return menuItemID;}
+    public double getPrice() {return price;}
+    public String getDescription() {return description;}
+    public Category getCategory() {return category;}
+    public String getImageUrl() {return imageUrl;}
+    public boolean getIsAvailable() {return isAvailable;}
+    public int getPreparationTime() {return preparationTime;}
+    public int getServingSize() {return servingSize;}
+    
+
+
+    public void setItemName(String itemName){
+        if(itemName != null && !itemName.trim().isEmpty()){
+            this.itemName = itemName;
+        } else {
+            throw new IllegalArgumentException("Item name cannot be null or empty.");
+        }
+    }
+
+    public void setIsAvailable() {this.isAvailable = true;}
+    public void setIsUnavailable() {this.isAvailable = false;}
+
+    public void setPrice(double price) {
+        if(price < 0.0){
+            throw new IllegalArgumentException("Price cannot be negative.");
+        }
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        if(description != null && !description.trim().isEmpty()){
+            this.description = description;
+        } else {
+            throw new IllegalArgumentException("Description cannot be null or empty.");
+        }
+    }
+
+    public void setCategory(Category category) {
+        if(category != null){
+            this.category = category;
+        } else {
+            throw new IllegalArgumentException("Category cannot be null.");
+        }
+    }
+
+    public void setImageUrl(String imageUrl) {
+        if(imageUrl != null && !imageUrl.trim().isEmpty()){
+            this.imageUrl = imageUrl;
+        } else {
+            throw new IllegalArgumentException("Image URL cannot be null or empty.");
+        }
+    }
+    public void setPreparationTime(int preparationTime) {
+        if(preparationTime < 0){
+            throw new IllegalArgumentException("Preparation time cannot be negative.");
+        }
+        this.preparationTime = preparationTime;
+    }
+    public void setServingSize(int servingSize) {
+        if(servingSize < 0){
+            throw new IllegalArgumentException("Serving size cannot be negative.");
+        }
+        this.servingSize = servingSize;
+    }
+
+
+}
---
+++ src/models/OrderItem.java
@@ -0,0 +1,35 @@
+package models;
+
+class OrderItem{
+    private MenuItem menuItem;
+    private int quantity;
+    private double subTotal;
+
+    public OrderItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+        this.quantity = 1;
+        this.subTotal = menuItem.getPrice() * quantity;
+    }
+
+    public void incrementQuantity() {
+        this.quantity++;
+        this.subTotal = menuItem.getPrice() * quantity;
+    }
+    public void decrementQuantity() {
+        if (quantity > 1) {
+            this.quantity--;
+            this.subTotal = menuItem.getPrice() * quantity;
+        }
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+}
